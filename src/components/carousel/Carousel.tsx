09118ccd3ee10885cc186f215b4ce173dcae6759
devanshu <devanshushresth3@gmail.com>
2026-01-18 13:46:17 +0530

    fix the issue of carousel.css for pre deployment

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import "../../styles/Carousel.css";
+import "../../styles/carousel.css";
 
 interface CarouselProps {
   images: string[];
