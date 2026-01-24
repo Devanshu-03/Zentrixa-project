745d9047857ed21b4ce39fb5eed3aaf929650fe3
devanshu <devanshushresth3@gmail.com>
2026-01-24 20:48:01 +0530

    fix the issue of testimonial and routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Zentrixa-project/', // <-- REQUIRED for GitHub Pages
+  base: '/Zentrixa-Technologies/', // <-- REQUIRED for GitHub Pages
   plugins: [
     react({
       babel: {
